f8ee18a5775c53a3bccd5fdbf45b0cb5f68c8cd3
omarMo7amed <om8190410@gmail.com>
2024-07-11 12:33:14 +0300

    renamed files

--- 03-signUpForm/script/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const container = document.querySelector(".container");
-const btnSignUp = document.querySelector(".green-bg button");
-
-btnSignUp.addEventListener("click", () => container.classList.toggle("change"));
